db1740d84d0f2f548a5f1b84e7df5b95d3fe10cb
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-26 19:07:27 +0900

    request camera permission

--- app/src/main/java/jp/cordea/cameraxdemo/MainActivity.kt
+++ app/src/main/java/jp/cordea/cameraxdemo/MainActivity.kt
@@ -1,12 +1,41 @@
 package jp.cordea.cameraxdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE = 1
+    }
+
+    private val isCameraEnabled: Boolean
+        get() = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isCameraEnabled) {
+            // start
+            return
+        }
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE) {
+            if (isCameraEnabled) {
+                // start
+                return
+            }
+        }
     }
 }
