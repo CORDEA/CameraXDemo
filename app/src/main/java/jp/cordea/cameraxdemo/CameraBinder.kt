01421832bfbd3832d2c40787cecd04491b45404f
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-01 10:16:25 +0900

    recognize text

--- app/src/main/java/jp/cordea/cameraxdemo/CameraBinder.kt
+++ app/src/main/java/jp/cordea/cameraxdemo/CameraBinder.kt
@@ -1,20 +1,30 @@
 package jp.cordea.cameraxdemo
 
 import android.graphics.Matrix
+import android.media.Image
 import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.lifecycle.LifecycleOwner
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 class CameraBinder(
     private val owner: LifecycleOwner,
     private val textureView: TextureView
 ) {
+    private val detector by lazy { FirebaseVision.getInstance().onDeviceTextRecognizer }
+
     fun start() {
         val preview = Preview(PreviewConfig.Builder().build())
+        val analysis = ImageAnalysis(ImageAnalysisConfig.Builder().build()).apply {
+            setAnalyzer { image, rotationDegrees ->
+                image?.image?.let {
+                    recognizeText(it, rotationDegrees)
+                }
+            }
+        }
 
         preview.setOnPreviewOutputUpdateListener {
             val parent = textureView.parent as ViewGroup
@@ -24,22 +34,39 @@ class CameraBinder(
             update()
         }
 
-        CameraX.bindToLifecycle(owner, preview)
+        CameraX.bindToLifecycle(owner, preview, analysis)
     }
 
     private fun update() {
         val display = textureView.display ?: return
         val x = textureView.width / 2f
         val y = textureView.height / 2f
-        val rotationDegrees = when (display.rotation) {
-            Surface.ROTATION_0 -> 0f
-            Surface.ROTATION_90 -> 90f
-            Surface.ROTATION_180 -> 180f
-            Surface.ROTATION_270 -> 270f
-            else -> throw IllegalStateException()
-        }
+        val rotationDegrees = display.rotation.fromRotation().toFloat()
         textureView.setTransform(
             Matrix().apply { postRotate(-rotationDegrees, x, y) }
         )
+    }
+
+    private fun recognizeText(image: Image, rotationDegrees: Int) {
+        val visionImage = FirebaseVisionImage.fromMediaImage(image, rotationDegrees.toRotation())
+        detector.processImage(visionImage)
+            .addOnSuccessListener {
+            }
+    }
+
+    private fun Int.toRotation() = when (this) {
+        0 -> Surface.ROTATION_0
+        90 -> Surface.ROTATION_90
+        180 -> Surface.ROTATION_180
+        270 -> Surface.ROTATION_270
+        else -> throw IllegalStateException()
+    }
+
+    private fun Int.fromRotation() = when (this) {
+        Surface.ROTATION_0 -> 0
+        Surface.ROTATION_90 -> 90
+        Surface.ROTATION_180 -> 180
+        Surface.ROTATION_270 -> 270
+        else -> throw IllegalStateException()
     }
 }
